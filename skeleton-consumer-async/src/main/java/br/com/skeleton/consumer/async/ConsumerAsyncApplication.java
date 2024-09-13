1cea618d5c42a5866e96b6711ff3ce24a58af33f
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-09-13 00:32:13 -0300

    Movendo pacote para consumer.async

--- skeleton-consumer-async/src/main/java/br/com/skeleton/consumer/async/ConsumerAsyncApplication.java
+++ skeleton-consumer-async/src/main/java/br/com/skeleton/consumer/async/ConsumerAsyncApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients(basePackages = "br.com.skeleton.romote")
+@EnableFeignClients(basePackages = "br.com.skeleton.consumer.async.romote")
 @SpringBootApplication
 public class ConsumerAsyncApplication {
 
