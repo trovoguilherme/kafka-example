daff28a88501ba1f3f9ac9fcb37bdbc1f9fbc657
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-09-13 00:24:03 -0300

    Adicionando chama para o feign client

--- skeleton-consumer-async/src/main/java/br/com/skeleton/ConsumerAsyncApplication.java
+++ skeleton-consumer-async/src/main/java/br/com/skeleton/ConsumerAsyncApplication.java
@@ -2,7 +2,9 @@ package br.com.skeleton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "br.com.skeleton.romote")
 @SpringBootApplication
 public class ConsumerAsyncApplication {
 
---
+++ skeleton-consumer-async/src/main/java/br/com/skeleton/romote/ExternalServiceApi.java
@@ -0,0 +1,15 @@
+package br.com.skeleton.romote;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
+
+@FeignClient(name = "externalServiceApi", url = "${remote.external-service-api.url}")
+public interface ExternalServiceApi {
+
+    @GetMapping("/{clientId}")
+    Optional<Void> findByClientId(@PathVariable String clientId);
+
+}
