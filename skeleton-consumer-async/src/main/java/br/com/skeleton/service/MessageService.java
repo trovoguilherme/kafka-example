c298f62ec9da6cde3dbed60eb64a5c58dca12c00
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-09-13 00:25:24 -0300

    Adicionando chamada para external service

---
+++ skeleton-consumer-async/src/main/java/br/com/skeleton/service/MessageService.java
@@ -0,0 +1,22 @@
+package br.com.skeleton.service;
+
+import br.com.skeleton.avro.Message;
+import br.com.skeleton.romote.ExternalServiceApi;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MessageService {
+
+    private final ExternalServiceApi externalServiceApi;
+
+    public void process(Message message) {
+        Optional<Void> byClientId = externalServiceApi.findByClientId(message.getId());
+    }
+
+}
