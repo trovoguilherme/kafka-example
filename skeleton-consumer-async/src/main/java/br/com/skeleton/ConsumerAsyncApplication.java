a9fdfff6348a8efe7029e3b321beb761793528fc
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-09-13 00:03:01 -0300

    Renomeando classes de tests

--- skeleton-consumer-async/src/main/java/br/com/skeleton/ConsumerAsyncApplication.java
+++ skeleton-consumer-async/src/main/java/br/com/skeleton/ConsumerAsyncApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConsumerAsyncApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerAsyncApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerAsyncApplication.class, args);
+    }
 
 }
