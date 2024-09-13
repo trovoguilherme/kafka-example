3a7e6df4c1350775ef0a1c312f4b0c3926c67b7b
Guilherme Trovo <xguilhermetrovo@gmail.com>
2024-09-13 00:25:11 -0300

    Adicionando chamada para external service

--- skeleton-consumer-async/src/main/java/br/com/skeleton/consumer/KafkaAvroConsumer.java
+++ skeleton-consumer-async/src/main/java/br/com/skeleton/consumer/KafkaAvroConsumer.java
@@ -1,6 +1,8 @@
 package br.com.skeleton.consumer;
 
 import br.com.skeleton.avro.Message;
+import br.com.skeleton.service.MessageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
@@ -9,12 +11,19 @@ import java.util.function.Consumer;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class KafkaAvroConsumer {
+
+    private final MessageService messageService;
 
     @Bean
     public Consumer<Message> messageConsumer() {
         return message -> {
             log.info("Message Received: {}", message);
+
+            messageService.process(message);
+
+            log.info("m=messageConsumer, msg=External Service called with success");
         };
     }
 
